704c35353e6229d51d6dc9033ea95cfaa9c7e18b
Victor Ferreira <vitfera@hotmail.com>
2025-12-29 23:17:07 -0300

    adicionando coluna 'Motivo da Não Anuência' na exportação Excel de propostas

--- src/Regmel/Service/ProposalService.php
+++ src/Regmel/Service/ProposalService.php
@@ -264,6 +264,7 @@ readonly class ProposalService extends AbstractEntityService implements Proposal
             $this->translator->trans('csv.header.total_value'),
             $this->translator->trans('csv.header.consented_by'),
             $this->translator->trans('csv.header.proposal_status'),
+            $this->translator->trans('csv.header.status_reason'),
             $this->translator->trans('csv.header.snpr_affiliation'),
             $this->translator->trans('csv.header.snpr_affiliation_details'),
             $this->translator->trans('csv.header.map_file'),
@@ -316,6 +317,13 @@ readonly class ProposalService extends AbstractEntityService implements Proposal
             $profitType = 'Organização da Sociedade Civil - OSC';
         }
 
+        // Motivo da não anuência - só mostra quando status é "Não Anuída"
+        $statusReason = '';
+        $currentStatus = $extraFields['status'] ?? '';
+        if ($currentStatus === StatusProposalEnum::NAO_ANUIDA->value) {
+            $statusReason = $extraFields['status_reason'] ?? '';
+        }
+
         return [
             $this->generateProposalCode($entity),
             $region,
@@ -329,6 +337,7 @@ readonly class ProposalService extends AbstractEntityService implements Proposal
             number_format($totalValue, 2, ',', '.'),
             $extraFields['status_updated_by_name'] ?? '---',
             $extraFields['status'] ?? '',
+            $statusReason,
             $extraFields['snpr_affiliation'] ?? 'Não',
             $extraFields['snpr_affiliation_details'] ?? '',
             $mapFileLink,
