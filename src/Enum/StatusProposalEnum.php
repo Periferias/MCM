f67699873892a3022cf5450cc4219c363f4fdc8a
Victor Ferreira <vitfera@hotmail.com>
2026-02-05 22:47:48 -0300

    Altera texto do status para 'Aguardando Validação da Anuência'

--- src/Enum/StatusProposalEnum.php
+++ src/Enum/StatusProposalEnum.php
@@ -13,7 +13,7 @@ enum StatusProposalEnum: string
     case ENVIADA = 'Enviada';
     case RECEBIDA = 'Recebida';
     case SEM_ADESAO = 'Sem Adesão do Município';
-    case AGUARDANDO_AVALIACAO_ANUENCIA = 'Aguardando Avaliação da Anuência';
+    case AGUARDANDO_AVALIACAO_ANUENCIA = 'Aguardando Validação da Anuência';
     case ANUIDA = 'Anuída';
     case NAO_ANUIDA = 'Não Anuída';
     case SELECIONADA = 'Selecionada';
