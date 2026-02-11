0bbbf7545d69bb60c17d695933b15573365d9f52
Victor Ferreira <vitfera@hotmail.com>
2026-02-11 12:13:17 -0300

    Altera texto dos status de anuência para especificar 'pelo Município'

--- src/Enum/StatusProposalEnum.php
+++ src/Enum/StatusProposalEnum.php
@@ -14,8 +14,8 @@ enum StatusProposalEnum: string
     case RECEBIDA = 'Recebida';
     case SEM_ADESAO = 'Sem Adesão do Município';
     case AGUARDANDO_AVALIACAO_ANUENCIA = 'Aguardando Validação da Anuência';
-    case ANUIDA = 'Anuída';
-    case NAO_ANUIDA = 'Não Anuída';
+    case ANUIDA = 'Anuída pelo Município';
+    case NAO_ANUIDA = 'Não Anuída pelo Município';
     case SELECIONADA = 'Selecionada';
     case NAO_SELECIONADA = 'Não Selecionada';
     case CLASSIFICADA = 'Classificada';
